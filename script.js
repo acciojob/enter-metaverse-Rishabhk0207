f534d29f3773496238c45289e466d96b0487713e
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2025-01-25 20:56:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const display = document.querySelector("#status");
 const btn = document.querySelector("#enterBtn");
 
 btn.addEventListener("click",()=>{
+	display.innerText = "Enter Metaverse"
 	display.innerHTML = `<h1> ${display.innerText} </h1>` ;
 })
