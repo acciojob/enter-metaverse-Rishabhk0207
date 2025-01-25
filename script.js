fb96274c012644cce5455dc0f43822f6beffad0f
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2025-01-25 20:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+const display = document.querySelector("#status");
+const btn = document.querySelector("#enterBtn");
+
+btn.addEventListener("click",()=>{
+	display.innerHTML = `<h1> ${display.innerText} </h1>` ;
+})
